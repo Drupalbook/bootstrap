871dcc23fe79af5456096070247470a4bf29c6cf
Ivan Abramenko <levmyshkin89@gmail.com>
2017-04-16 14:36:48 +0600

    Initial commit

---
+++ sites/default/settings.local.php
@@ -0,0 +1,11 @@
+<?php
+
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
+
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+
+$settings['extension_discovery_scan_tests'] = FALSE;
